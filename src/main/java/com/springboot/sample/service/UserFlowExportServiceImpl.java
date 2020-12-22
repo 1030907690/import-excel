90f55fc4e5474769803170d755d902dc18f961b3
1030907690@qq.com <1030907690@qq.com>
2020-12-22 14:49:01 +0800

    设置属性

--- src/main/java/com/springboot/sample/service/UserFlowExportServiceImpl.java
+++ src/main/java/com/springboot/sample/service/UserFlowExportServiceImpl.java
@@ -2,20 +2,16 @@ package com.springboot.sample.service;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
+import com.alibaba.fastjson.JSONObject;
 import com.springboot.sample.bean.CustomerService;
 import com.springboot.sample.bean.UserFlowCallAWrapper;
 import com.springboot.sample.bean.UserFlowFourWrapper;
 import com.springboot.sample.bean.UserFlowMeiQiaWrapper;
+import com.springboot.sample.common.type.MatchResultCauseEnum;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
@@ -27,24 +23,22 @@ public class UserFlowExportServiceImpl {
         TemplateExportParams params = new TemplateExportParams(templateUrl);
         params.setSheetNum(new Integer[]{0, 1, 2, 3});
         Map<String, Object> map = new HashMap<>();
-        List<Map<String, Object>> listMap = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            Map<String, Object> lm = new HashMap<>();
-            lm.put("id", i + 1);
-            lm.put("date", "2020/12/1" + i);
-            listMap.add(lm);
-        }
-        map.put("meiQiaList", listMap);
-
+        List<Map<String, Object>> meiQiaListMap = new ArrayList<>();
+        meiQiaSheetSetPropertyValues(meiQiaListMap, userFlowMeiQiaWrapperList);
+        map.put("meiQiaList", meiQiaListMap);
 
         List<Map<String, Object>> fourListMap = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            Map<String, Object> lm = new HashMap<>();
-            lm.put("id", i + 2);
-            lm.put("date", "2020/12/1" + i);
-            fourListMap.add(lm);
-        }
+        fourSheetSetPropertyValues(fourListMap, userFlowFourWrapperList);
         map.put("fourList", fourListMap);
+
+
+        List<Map<String, Object>> callAListMap = new ArrayList<>();
+        callASheetSetPropertyValues(callAListMap, userFlowCallAWrapperList);
+        map.put("callAList", callAListMap);
+
+        List<Map<String, Object>> waitHandlerListMap = new ArrayList<>();
+        waitHandlerSheetSetPropertyValues(waitHandlerListMap, waitHandlerList);
+        map.put("waitHandlerList", waitHandlerListMap);
 
         Workbook workbook = ExcelExportUtil.exportExcel(params, map);
        /* File file = new File(System.getProperty("user.dir"), "out");
@@ -61,11 +55,45 @@ public class UserFlowExportServiceImpl {
         return workbook;
     }
 
+    private void waitHandlerSheetSetPropertyValues(List<Map<String, Object>> waitHandlerListMap, List<CustomerService> waitHandlerList) {
+        for (CustomerService waitHandler : waitHandlerList) {
+            Map<String, Object> object = JSONObject.parseObject(JSONObject.toJSONString(waitHandler), Map.class);
+            waitHandlerListMap.add(object);
+        }
+    }
+
+    private void callASheetSetPropertyValues(List<Map<String, Object>> callAListMap, List<UserFlowCallAWrapper> userFlowCallAWrapperList) {
+        for (UserFlowCallAWrapper userFlowCallAWrapper : userFlowCallAWrapperList) {
+            if (MatchResultCauseEnum.SUCCESS == userFlowCallAWrapper.getMatchResult().getMatchResultCauseEnum()) {
+                Map<String, Object> object = JSONObject.parseObject(JSONObject.toJSONString(userFlowCallAWrapper), Map.class);
+                callAListMap.add(object);
+            }
+        }
+    }
+
+    private void fourSheetSetPropertyValues(List<Map<String, Object>> fourListMap, List<UserFlowFourWrapper> userFlowFourWrapperList) {
+        for (UserFlowFourWrapper userFlowFourWrapper : userFlowFourWrapperList) {
+            if (MatchResultCauseEnum.SUCCESS == userFlowFourWrapper.getMatchResult().getMatchResultCauseEnum()) {
+                Map<String, Object> object = JSONObject.parseObject(JSONObject.toJSONString(userFlowFourWrapper), Map.class);
+                fourListMap.add(object);
+            }
+        }
+    }
+
+    private void meiQiaSheetSetPropertyValues(List<Map<String, Object>> meiQiaListMap, List<UserFlowMeiQiaWrapper> userFlowMeiQiaWrapperList) {
+        for (UserFlowMeiQiaWrapper userFlowMeiQiaWrapper : userFlowMeiQiaWrapperList) {
+            if (MatchResultCauseEnum.SUCCESS == userFlowMeiQiaWrapper.getMatchResult().getMatchResultCauseEnum()) {
+                Map<String, Object> object = JSONObject.parseObject(JSONObject.toJSONString(userFlowMeiQiaWrapper), Map.class);
+                meiQiaListMap.add(object);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         UserFlowExportServiceImpl userFlowExportServiceImpl = new UserFlowExportServiceImpl();
         try {
-            userFlowExportServiceImpl.userFlowExportExcel(userFlowExportServiceImpl.templateUrl,null,null,null,null);
+            userFlowExportServiceImpl.userFlowExportExcel(userFlowExportServiceImpl.templateUrl, null, null, null, null);
         } catch (IOException e) {
             e.printStackTrace();
         }
